12dbccd2b7e2bf5fdcdf173ba7d50bbd6876b160
zhounbing1 <1071594554@qq.com>
2017-08-05 09:53:43 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/tangcco/temp056/HelloWordTest.java
@@ -0,0 +1,18 @@
+package cn.bdqn.tangcco.temp056;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/8/5.
+ */
+public class HelloWordTest {
+
+    @Test
+    public   void print(){
+
+        System.out.println("hello Word");
+        String result =null;
+        System.out.println("人比人气死人！");
+    }
+
+}
